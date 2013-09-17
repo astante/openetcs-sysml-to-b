2926db52ed6f970893f0a979088814818b25d637
Alexander Stante <alexander.stante@esk.fraunhofer.de>
2013-09-17 12:53:12 +0200

    Added generation of comments for SysML Blocks.

--- de.fraunhofer.esk.openetcs.sysml2b/src/de/fraunhofer/esk/openetcs/sysml2b/FileGenerator.java
+++ de.fraunhofer.esk.openetcs.sysml2b/src/de/fraunhofer/esk/openetcs/sysml2b/FileGenerator.java
@@ -6,16 +6,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 
+
+import java.util.HashMap;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.papyrus.sysml.blocks.Block;
+import org.eclipse.uml2.uml.Comment;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Model;
 import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Stereotype;
+
+
 // import org.eclipse.uml2.uml.util.UMLUtil;
 import de.fraunhofer.esk.openetcs.sysml2b.StructureMapping;
 
@@ -36,12 +42,25 @@ public class FileGenerator {
 
 
 	public void generateAndWrite() {
+		HashMap<Element, Comment> mapping = new HashMap<Element, Comment>();
+		
+		// Build a mapping of <Element> to <Comment> to find the associated
+		// comments fast in the transformation
+		for (Element element: sysmlModel.allOwnedElements()) {
+			if (element instanceof Comment) {
+				Comment comment = (Comment) element;
+				
+				for (Element e : comment.getAnnotatedElements()) {
+					mapping.put(e,  comment);
+				}
+			}
+		}
 
 		for (Element element: sysmlModel.allOwnedElements()) {
 			Stereotype stereotype;
 
 			if ((stereotype = element.getAppliedStereotype("SysML::Blocks::Block")) != null) {
-				createMachineFile((Block) element.getStereotypeApplication(stereotype));
+				createMachineFile((Block) element.getStereotypeApplication(stereotype), mapping.get(element));
 			}
 		}
 	}
@@ -56,10 +75,10 @@ public class FileGenerator {
 		return model;
 	}
 	
-	private void createMachineFile(Block block) {
+	private void createMachineFile(Block block, Comment comment) {
 		StructureMapping mapping = new StructureMapping();
 		
-		writeToFile(block.getBase_Class().getName() + ".mch", mapping.createMachine(block).toString());
+		writeToFile(block.getBase_Class().getName() + ".mch", mapping.createMachine(block, comment).toString());
 	}
 	
 	public void writeToFile(String filename, String content) {
