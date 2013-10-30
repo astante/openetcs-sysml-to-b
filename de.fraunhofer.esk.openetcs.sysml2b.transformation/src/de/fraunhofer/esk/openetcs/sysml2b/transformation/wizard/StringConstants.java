9e90606734c45bd230969e104afba4f9887a26bf
Alexander Stante <alexander.stante@esk.fraunhofer.de>
2013-10-30 16:55:13 +0100

    Typo fixed.

--- de.fraunhofer.esk.openetcs.sysml2b.transformation/src/de/fraunhofer/esk/openetcs/sysml2b/transformation/wizard/StringConstants.java
+++ de.fraunhofer.esk.openetcs.sysml2b.transformation/src/de/fraunhofer/esk/openetcs/sysml2b/transformation/wizard/StringConstants.java
@@ -10,5 +10,5 @@ public interface StringConstants {
 	
 	public final String UI_MESSAGE_TITLE = "Project creation";
 	public final String UI_MESSAGE_PROJECT_EXISTS = "A Project with the same name already exists. Do you want to override it?";
-	public final String uI_MESSAGE_ERRORS_IN_MODEL = "The SysML model contains some errors. Do you really want to generate the Classical B Model?";
+	public final String UI_MESSAGE_ERRORS_IN_MODEL = "The SysML model contains some errors. Do you really want to generate the Classical B Model?";
 }
